8387007e57bfedffe5f9bf314a18a119e4a4087e
Lucas <lucas@agtlucas.com>
2016-07-10 20:54:23 -0300

    Update tests

---
+++ test/index.js
@@ -0,0 +1,19 @@
+const postcss = require('postcss')
+const expect = require('chai').expect
+const plugin = require('../')
+
+const test = (input, output, otps, done) => {
+  postcss([plugin(opts)]).process(input).then((result) => {
+    expect(result.css).to.eql(output)
+    expect(result.warnings()).to.be.empty
+    done()
+  }).catch((error) => {
+    done(error)
+  })
+}
+
+describe('postcss-z', () => {
+  it('Should replace `z` with `z-index`', done => {
+    test('.blackbriar { z: 1 }', '.blackbriar { z-index: 1 }', {}, done)
+  })
+})
