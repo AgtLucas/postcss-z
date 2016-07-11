802193ba5fba5db03352f2db4edfa2df9efa2fb1
Lucas <lucas@agtlucas.com>
2016-07-10 21:03:23 -0300

    Improve tests

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@ const expect = require('chai').expect
 const plugin = require('../')
 
 const test = (input, output, opts, done) => {
-  postcss([plugin(opts)]).process(input).then((result) => {
+  postcss([plugin(opts)]).process(input).then(result => {
     expect(result.css).to.eql(output)
     expect(result.warnings()).to.be.empty
     done()
-  }).catch((error) => {
+  }).catch(error => {
     done(error)
   })
 }
